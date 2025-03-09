8c5efaa13ce643fb962b491d7be9e37bf5274f7b
TLRN <lrn.tl.dv@gmail.com>
2025-03-09 02:26:53 +0200

    feat: Add timing for GPU-accelerated function execution to measure performance impact

--- src/gpu/getting_started.py
+++ src/gpu/getting_started.py
@@ -1,13 +1,24 @@
 from numba import jit
 import numpy as np
+import time
 
 x = np.arange(100).reshape(10, 10)
 
-@jit(nopython=True) # Set "nopython" mode for best performance, equivalent to @njit
-def go_fast(a): # Function is compiled to machine code when called the first time
+@jit(nopython=True)
+def go_fast(a): # Function is compiled and runs in machine code
     trace = 0.0
-    for i in range(a.shape[0]):   # Numba likes loops
-        trace += np.tanh(a[i, i]) # Numba likes NumPy functions
-    return a + trace              # Numba likes NumPy broadcasting
+    for i in range(a.shape[0]):
+        trace += np.tanh(a[i, i])
+    return a + trace
 
-print(go_fast(x))
+# DO NOT REPORT THIS... COMPILATION TIME IS INCLUDED IN THE EXECUTION TIME!
+start = time.time()
+go_fast(x)
+end = time.time()
+print("Elapsed (with compilation) = %s" % (end - start))
+
+# NOW THE FUNCTION IS COMPILED, RE-TIME IT EXECUTING FROM CACHE
+start = time.time()
+go_fast(x)
+end = time.time()
+print("Elapsed (after compilation) = %s" % (end - start))
